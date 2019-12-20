0eea6ed4a3f3904c9e8fb888f78f6ce3fc47cda1
kpmnsasindu <kpmnsasindu@gmail.com>
2019-12-21 00:42:29 +0530

    Simple Calculator

---
+++ src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,14 @@
+package simplecalculator;
+
+/**
+ *
+ * @author acer
+ */
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.setVisible(true);
+    }
+    
+}
